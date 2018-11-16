965e938c94b661e41959bc342efc4ab6c1df0017
Jan Odjas <jan.odjas@gmail.com>
2018-11-16 16:27:00 +0100

    basic methods and fields sketches created.

--- src/ConsoleReader.java
+++ src/ConsoleReader.java
@@ -1,2 +1,4 @@
 public class ConsoleReader {
+    // klasa do komunikacji z uzytkownikiem tu beda wszystkie metody do obslugi wprowadzania konkretnych danych.
+
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,16 @@
 public class Menu {
+    //zawiera wszytskie odwoania do funkcji programu
+
+    public void newSearch(){
+
+    }
+
+    public void saveSession(){
+
+    }
+
+    public void exit(){
+
+    }
+
 }
