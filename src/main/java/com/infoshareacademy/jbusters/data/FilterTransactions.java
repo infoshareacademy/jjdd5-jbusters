cc61b22496458600d22bb57291b9d8bd9d13cb20
Jan Odjas <jan.odjas@gmail.com>
2018-11-28 17:10:52 +0100

    add removeInvalidTransaction method, FilterSelector method WiP

--- src/main/java/com/infoshareacademy/jbusters/data/FilterTransactions.java
+++ src/main/java/com/infoshareacademy/jbusters/data/FilterTransactions.java
@@ -2,12 +2,15 @@ package com.infoshareacademy.jbusters.data;
 
 import java.math.BigDecimal;
 import java.util.*;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class FilterTransactions {
 
     private List<Transaction> transactionsBase = new ArrayList<>();
+    BigDecimal areaDiff = new BigDecimal(20);
+    BigDecimal areaDiffExpanded = new BigDecimal(25);
+    int minResultsNumber = 11;
+    BigDecimal priceDiff = new BigDecimal(600.0);
 
     public FilterTransactions(ArrayList<Transaction> transactionsData) {
 
@@ -17,14 +20,15 @@ public class FilterTransactions {
     // metoda zwracajaca liste tranzakcji, ktora jest wynikiem wielokrotnego przefiltrowania gwnej bazy tranzakcji
     //kolejnosc filtrow:  data tranzakcji/miasto/dzielnica/rynek/kategoria budowy/powierzchnia mieszkania
 
-    public List<Transaction> theGreatFatFilter(Transaction userTransaction){
-
-        if(basicFilter(userTransaction).size()>11){
-
-        }
+    public List<Transaction> theGreatFatFilter(Transaction userTransaction) {
         return new ArrayList<Transaction>();
     }
 
+
+    private List<Transaction> notEnoughtResultsAction() {
+        System.out.println("Baza zawiera zbyt mala ilosc pasujacych transakcji");
+        return new ArrayList<Transaction>();
+    }
 
     private List<Transaction> basicFilter(Transaction userTransaction) {
         List<Transaction> lista = transactionsBase.stream()
@@ -37,7 +41,42 @@ public class FilterTransactions {
         return lista;
     }
 
-    private List<Transaction> districtFilter(List<Transaction> transactionsBase, Transaction userTransaction, boolean districtWeightRespected) {
+    //fixme na razie nie dzialam;
+    //TODO wprowadzic rekurencje selectorfilter!!!
+    private List<Transaction> selectorFilter(boolean isSingleDistrict, boolean isAreaDiffSmall, List<Transaction> transactionsList, Transaction userTransaction){
+                if(isSingleDistrict){
+                    List<Transaction> singleDistrictFilter=singleDistrictFilter(transactionsList,userTransaction);
+                    if(isAreaDiffSmall){
+                        List<Transaction> flatAreafilter = flatAreaFilter(singleDistrictFilter,userTransaction,areaDiff);
+                        flatAreafilter = invalidTransactionsRemover(flatAreafilter);
+
+                        if(isEnoughtResults(flatAreafilter,minResultsNumber)){
+                            return flatAreafilter;
+                        }
+                        else{
+
+                        }
+                    }
+                    else{
+                        return flatAreaFilter(singleDistrictFilter,userTransaction,areaDiffExpanded);
+                    }
+                }
+                else{
+                    List<Transaction> MultiDistrictFilter=singleDistrictFilter(transactionsList,userTransaction);
+                    if(isAreaDiffSmall){
+                        return flatAreaFilter(MultiDistrictFilter,userTransaction,areaDiff);
+                    }
+                    else{
+                        return flatAreaFilter(MultiDistrictFilter,userTransaction,areaDiffExpanded);
+                    }
+                }
+
+        return transactionsList;
+    }
+
+
+
+    private List<Transaction> singleDistrictFilter(List<Transaction> transactionsBase, Transaction userTransaction) {
         List<Transaction> lista = transactionsBase.stream()
 
                 .filter(transaction -> transaction.getDistrict().equalsIgnoreCase(userTransaction.getDistrict()))
@@ -46,7 +85,16 @@ public class FilterTransactions {
         return new ArrayList(lista);
     }
 
-    private List<Transaction> flatArrayFilter(List<Transaction> transactionsBase, Transaction userTransaction, BigDecimal areaDiff) {
+    private List<Transaction> multiDistrictFilter(List<Transaction> transactionsBase, Transaction userTransaction) {
+        List<Transaction> lista = transactionsBase.stream()
+
+                .filter(transaction -> transaction.getDistrict().equalsIgnoreCase(userTransaction.getDistrict()))
+                .collect(Collectors.toList());
+
+        return new ArrayList(lista);
+    }
+
+    private List<Transaction> flatAreaFilter(List<Transaction> transactionsBase, Transaction userTransaction, BigDecimal areaDiff) {
         List<Transaction> lista = transactionsBase.stream()
 
                 .filter(transaction -> ((transaction.getFlatArea()).compareTo(userTransaction.getFlatArea().add(areaDiff))) <= 0)
@@ -64,19 +112,19 @@ public class FilterTransactions {
                 .sorted((o1, o2) -> o1.getPricePerM2().compareTo(o2.getPricePerM2()))
                 .collect(Collectors.toList());
 
-        removeLeftOutliers(transSortedByPPerM2,maxDiff);
-        removeRightOutliers(transSortedByPPerM2,maxDiff);
+        removeLeftOutliers(transSortedByPPerM2, maxDiff);
+        removeRightOutliers(transSortedByPPerM2, maxDiff);
         return transSortedByPPerM2;
     }
 
-    private void removeLeftOutliers(List<Transaction> transSortedByPPerM2, BigDecimal maxDiff){
+    private void removeLeftOutliers(List<Transaction> transSortedByPPerM2, BigDecimal maxDiff) {
 
         BigDecimal firstPPM2 = getPricePerMeter(transSortedByPPerM2, 0);
         BigDecimal secondPPM2 = getPricePerMeter(transSortedByPPerM2, 1);
         if (isPriceDifferenceToBig(firstPPM2, secondPPM2, maxDiff)) transSortedByPPerM2.remove(0);
     }
 
-    private void removeRightOutliers(List<Transaction> transSortedByPPerM2, BigDecimal maxDiff){
+    private void removeRightOutliers(List<Transaction> transSortedByPPerM2, BigDecimal maxDiff) {
         int lastIndex = transSortedByPPerM2.size() - 1;
         BigDecimal lastPPM2 = getPricePerMeter(transSortedByPPerM2, lastIndex);
         BigDecimal secondToLastPPM2 = getPricePerMeter(transSortedByPPerM2, lastIndex - 1);
@@ -85,6 +133,31 @@ public class FilterTransactions {
 
     private boolean isPriceDifferenceToBig(BigDecimal firstPrice, BigDecimal secondPrice, BigDecimal maxDiff) {
         return secondPrice.subtract(firstPrice).compareTo(maxDiff) > 0;
+    }
+
+    private List<Transaction> invalidTransactionsRemover(List<Transaction> finallySortedList) {
+        finallySortedList = removeOutliers(finallySortedList,priceDiff);
+
+        boolean isRemoved = false;
+        while (isRemoved) {
+            if (!isCheapestTransactionValid(finallySortedList)) {
+                finallySortedList.remove(getLessExpensiveFlat(finallySortedList));
+                isRemoved = true;
+            } else {
+                isRemoved = false;
+            }
+        }
+        isRemoved = false;
+        while (isRemoved) {
+            if (!isMostExpensiveTransactionValid(finallySortedList)) {
+                finallySortedList.remove(getLessExpensiveFlat(finallySortedList));
+                isRemoved = true;
+            } else {
+                isRemoved = false;
+            }
+        }
+
+        return finallySortedList;
     }
 
     //TODO porownac enumy z danej listy i na ich podastawie wyszanczyc minimalne wartosci pol w danym zbiorze, tania transakcja powinna miec minimalne pola jesli nie to wywalamy ja
@@ -119,71 +192,79 @@ public class FilterTransactions {
     private boolean isWorstParkingPlace(List<Transaction> finallySortedList, Transaction transToCheck) {
 
         ParkingPlace worstParking = finallySortedList.stream()
+                .skip(1)
                 .map(x -> ParkingPlace.valueOf(x.getParkingSpot()))
                 .distinct().min(new ParkingPlaceComparator())
                 .orElse(ParkingPlace.BRAK_MP);
-        return ParkingPlace.valueOf(transToCheck.getParkingSpot()).compareTo(worstParking) == 0;
+        return ParkingPlace.valueOf(transToCheck.getParkingSpot()).compareTo(worstParking) <= 0;
 
     }
 
     private boolean isBestParkingPlace(List<Transaction> finallySortedList, Transaction transToCheck) {
 
         ParkingPlace bestParking = finallySortedList.stream()
+                .limit(finallySortedList.size() - 1)
                 .map(x -> ParkingPlace.valueOf(x.getParkingSpot()))
                 .distinct().max(new ParkingPlaceComparator())
                 .orElse(ParkingPlace.GARAZ);
-        return ParkingPlace.valueOf(transToCheck.getParkingSpot()) == bestParking;
+        return ParkingPlace.valueOf(transToCheck.getParkingSpot()).compareTo(bestParking) >= 0;
 
     }
 
     private boolean isWorstStandardLevel(List<Transaction> finallySortedList, Transaction transToCheck) {
         StandardLevel worstStandard = finallySortedList.stream()
+                .skip(1)
                 .map(x -> StandardLevel.valueOf(x.getStandardLevel()))
                 .distinct().min(new StandardLevelComparator())
                 .orElse(StandardLevel.DEWELOPERSKI);
-        return StandardLevel.valueOf(transToCheck.getStandardLevel()) == worstStandard;
+        return StandardLevel.valueOf(transToCheck.getStandardLevel()).compareTo(worstStandard) <= 0;
     }
 
     private boolean isBestStandardLevel(List<Transaction> finallySortedList, Transaction transToCheck) {
         StandardLevel bestStandard = finallySortedList.stream()
+                .limit(finallySortedList.size() - 1)
                 .map(x -> StandardLevel.valueOf(x.getStandardLevel()))
                 .distinct().max(new StandardLevelComparator())
                 .orElse(StandardLevel.WYSOKI);
-        return StandardLevel.valueOf(transToCheck.getStandardLevel()) == bestStandard;
+        return StandardLevel.valueOf(transToCheck.getStandardLevel()).compareTo(bestStandard) >= 0;
     }
 
     private boolean isWorstLevel(List<Transaction> finallySortedList, Transaction transToCheck) {
         int worstLevel = finallySortedList.stream()
+                .skip(1)
                 .mapToInt(Transaction::getLevel)
                 .min()
                 .orElse(0);
-        return transToCheck.getLevel() == worstLevel;
+        return transToCheck.getLevel() <= worstLevel;
     }
 
     private boolean isBestLevel(List<Transaction> finallySortedList, Transaction transToCheck) {
         int bestLevel = finallySortedList.stream()
+                .limit(finallySortedList.size() - 1)
                 .mapToInt(Transaction::getLevel)
                 .max()
                 .orElse(0);
-        return transToCheck.getLevel() == bestLevel;
+        return transToCheck.getLevel() >= bestLevel;
     }
 
     private boolean isWorstFlatArea(List<Transaction> finallySortedList, Transaction transToCheck) {
         BigDecimal worstFlatArea = finallySortedList.stream()
+                .skip(1)
                 .map(Transaction::getFlatArea)
                 .distinct()
                 .min(BigDecimal::compareTo)
                 .orElse(new BigDecimal(0.0));
-        return transToCheck.getFlatArea().compareTo(worstFlatArea) == 0;
+        return transToCheck.getFlatArea().compareTo(worstFlatArea) <= 0;
     }
 
     private boolean isBestFlatArea(List<Transaction> finallySortedList, Transaction transToCheck) {
         BigDecimal worstFlatArea = finallySortedList.stream()
+                .limit(finallySortedList.size() - 1)
                 .map(Transaction::getFlatArea)
                 .distinct()
                 .max(BigDecimal::compareTo)
                 .orElse(new BigDecimal(0.0));
-        return transToCheck.getFlatArea().compareTo(worstFlatArea) == 0;
+        return transToCheck.getFlatArea().compareTo(worstFlatArea) >= 0;
     }
 
 
@@ -192,6 +273,9 @@ public class FilterTransactions {
     }
 
 
+    private boolean isEnoughtResults(List<Transaction> listToCheck, int minSize) {
+        return listToCheck.size() >= minSize;
+    }
 
     //TODO:wpisac metode do wypisywania dzielnic
     public List<String> getDistrictsWithSameWeight(String inputDistrict) {
