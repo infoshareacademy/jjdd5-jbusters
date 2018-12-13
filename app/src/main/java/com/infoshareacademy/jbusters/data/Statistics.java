22a87c3aaecbf2cac915dd23dab12c11bb34a6a1
lukaszmarwitz <lukaszmarwitz@gmail.com>
2018-12-13 17:08:10 +0100

    update

--- app/src/main/java/com/infoshareacademy/jbusters/data/Statistics.java
+++ app/src/main/java/com/infoshareacademy/jbusters/data/Statistics.java
@@ -2,20 +2,32 @@ package com.infoshareacademy.jbusters.data;
 
 public class Statistics {
 
-    private String statisticName;
+    private String cityName;
+    private String districtName;
     private int counter;
+    private double averageValue;
 
     @Override
     public String toString() {
-        return "Dzielnica " + statisticName + " była użyta " + counter + " raz(y) w wycenie nieruchomości";
+        return  "Miasto " + cityName + " było użyte " + counter + " raz(y) w wycenie nieruchomości.\n" +
+                "Dzielnica " + districtName + " była użyta " + counter + " raz(y) w wycenie nieruchomości.\n" +
+                "Średnia cena nieruchomości w tym rejonie wynosi " + averageValue + " PLN";
     }
 
-    public String getStatisticName() {
-        return statisticName;
+    public String getCityName() {
+        return cityName;
     }
 
-    public void setStatisticName(String statisticName) {
-        this.statisticName = statisticName;
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
     }
 
     public int getCounter() {
@@ -24,5 +36,13 @@ public class Statistics {
 
     public void setCounter(int counter) {
         this.counter = counter;
+    }
+
+    public double getAverageValue() {
+        return averageValue;
+    }
+
+    public void setAverageValue(double averageValue) {
+        this.averageValue = averageValue;
     }
 }
