7f971e6ca7bc21bb2e5db265e2362f422cc9fdf3
TomaszOciepa <45019900+TomaszOciepa@users.noreply.github.com>
2018-12-31 08:31:34 +0100

    JB5JB-29 (#40)
    * loading a sql file into the database on the docker
    
    * Modification of the sql file adding an example table users
    
    * adding a registration form, checking if the user exists in the database
    
    * modify checking if the user exists
    
    *  Changes to be committed:
    	zmodyfikowany: web/src/main/webapp/index.html
    	nowy plik:     web/src/main/webapp/login-user.html
    
    * add login servlet
    
    * Adding login and logout from google api. Adding a logged-in user panel
    
    * Valuation of the flat of the logged in user
    
    * adding templates for the logged in user
    
    * Repair logout and improvement of jenkins comments
    
    * Improvement of jenkins comments
    
    * Improvement of Product owner comments

--- web/src/main/java/com/infoshareacademy/jbusters/web/UploadFileServlet.java
+++ web/src/main/java/com/infoshareacademy/jbusters/web/UploadFileServlet.java
@@ -1,7 +1,5 @@
 package com.infoshareacademy.jbusters.web;
 
-
-import com.infoshareacademy.jbusters.data.SearchOfData;
 import com.infoshareacademy.jbusters.freemarker.TemplateProvider;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -14,17 +12,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-@WebServlet(urlPatterns = ("/load-district"))
-public class LoadDistricTransactionServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/upload-file")
+public class UploadFileServlet extends HttpServlet {
 
-    private static final String TEMPLATE_NAME = "load-district";
-    private static final Logger LOG = LoggerFactory.getLogger(LoadDistricTransactionServlet.class);
+    private static final String TEMPLATE_NAME = "user-upload-file";
+    private static final Logger LOG = LoggerFactory.getLogger(UploadFileServlet.class);
 
     @Inject
     private TemplateProvider templateProvider;
@@ -35,20 +33,22 @@ public class LoadDistricTransactionServlet extends HttpServlet {
         resp.addHeader("Content-Type", "text/html; charset=utf-8");
         PrintWriter out = resp.getWriter();
 
-        String city = req.getParameter("city");
-        List<String> districtsList = new SearchOfData().showDistrict(city);
-
         Map<String, Object> model = new HashMap<>();
-        model.put("city", city);
-        model.put("district", districtsList);
+
+        HttpSession session = req.getSession(true);
+        String sessionEmail = (String) session.getAttribute("userEmail");
+        String sessionName = (String) session.getAttribute("userName");
 
         Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
+        model.put("sessionEmail", sessionEmail);
+        model.put("sessionName", sessionName);
 
         try {
             template.process(model, out);
-            LOG.info("Loaded district list of size {}", districtsList.size());
+            LOG.info("Loaded file");
         } catch (TemplateException e) {
-            LOG.error("Failed to load district list. Size of list: {}" + districtsList.size());
+            LOG.error("Failed to load file");
         }
+
     }
 }
