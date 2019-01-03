43fff2c351e3fd6cb4b390214cff25de51b4fb8e
Łukasz <43676193+lukaszmarwitz@users.noreply.github.com>
2019-01-03 12:32:24 +0100

    JD5JB-45-TranslationAndFixes (#42)
    * translation of first page, removed duplicated dependencies from app and web pom.xml, updated readme.md
    
    * run_app.sh correction

--- app/run_app.sh
+++ app/run_app.sh
@@ -1,3 +1,3 @@
 cp -r ./data ./target/
 cp app.properties ./target/
-java -jar ./target/jjdd5-jbusters-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar ./target/app-1.0-SNAPSHOT-jar-with-dependencies.jar
---
+++ app/src/main/java/com/infoshareacademy/jbusters/data/LanguageManager.java
@@ -0,0 +1,70 @@
+package com.infoshareacademy.jbusters.data;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+@ApplicationScoped
+public class LanguageManager {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LanguageManager.class);
+    private static final String LANGUAGE_KEY = "language";
+    private static final String LANGUAGE_DEFAULT_VALUE = "pl";
+    private static final Properties languageProperties = new Properties();
+
+    public String translate(String translationKey) throws IOException {
+
+        Locale currentLocale = new Locale(getLanguage());
+        ResourceBundle messages = ResourceBundle.getBundle("translations", currentLocale);
+
+        return messages.getString(translationKey);
+    }
+
+    public String getLanguage() throws IOException {
+
+        if (!Files.exists(StaticFields.getLangPropertiesPath())) {
+            Files.createFile(StaticFields.getLangPropertiesPath());
+        }
+
+        String url = StaticFields.getLangPropertiesPath().toString();
+        FileInputStream fis = new FileInputStream(url);
+
+        try {
+            languageProperties.load(fis);
+            fis.close();
+        } catch (IOException e) {
+            LOGGER.error("Missing properties file in path {}", StaticFields.getLangPropertiesPath().toString());
+        }
+
+        String language = languageProperties.getProperty(LANGUAGE_KEY, LANGUAGE_DEFAULT_VALUE);
+
+        LOGGER.info("Language set to default: {}", LANGUAGE_DEFAULT_VALUE);
+
+        return language;
+    }
+
+    public void setLanguage(String language) throws IOException {
+
+        if (!Files.exists(StaticFields.getLangPropertiesPath())) {
+            Files.createFile(StaticFields.getLangPropertiesPath());
+        }
+
+        String url = StaticFields.getLangPropertiesPath().toString();
+        FileOutputStream fos = new FileOutputStream(url);
+
+        languageProperties.setProperty(LANGUAGE_KEY, language);
+        languageProperties.store(fos, "Language Properties");
+
+        fos.close();
+
+        LOGGER.info("Language stored and set to: {}", language);
+    }
+}
