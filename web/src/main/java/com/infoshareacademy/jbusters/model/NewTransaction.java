dd8f532a86c5e533ac578f7831ee9adedc114c47
Karol Bemka <karolkarwia@gmail.com>
2019-01-07 19:39:51 +0100

    Edited popup messages for poslih language only

---
+++ web/src/main/java/com/infoshareacademy/jbusters/model/NewTransaction.java
@@ -0,0 +1,235 @@
+package com.infoshareacademy.jbusters.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "new_transaction")
+public class NewTransaction {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "NEW_TRANSACTION_ID")
+    private int newTransactionId;
+
+    @ManyToOne
+    @JoinColumn(name = "NEW_TRANSACTION_USER_ID")
+    private User newTransactionUserId;
+
+    @Column(name = "NEW_TRANSACTION_DESCRIPTION")
+    private String newTransactionDescription;
+
+    @Column(name = "NEW_TRANSACTION_SALE")
+    private String newTransactionSale;
+
+    @Column(name = "NEW_TRANSACTION_IMPORTANT")
+    private String newTransactionImportant;
+
+    @Column(name = "NEW_TRANSACTION_DATA_TRANSACTION")
+    private LocalDate newTransactionDataTransaction;
+
+    @Column(name = "NEW_TRANSACTION_CITY")
+    private String newTransactionCity;
+
+    @Column(name = "NEW_TRANSACTION_DISTRICT")
+    private String newTransactionDistrict;
+
+    @Column(name = "NEW_TRANSACTION_STREET")
+    private String newTransactionStreet;
+
+    @Column(name = "NEW_TRANSACTION_TYPE_OF_MARKET")
+    private String newTransactionTypeOfMarket;
+
+    @Column(name = "NEW_TRANSACTION_PRICE")
+    private BigDecimal newTransactionPrice;
+
+    @Column(name = "NEW_TRANSACTION_FLAT_AREA")
+    private BigDecimal newTransactionFlatArea;
+
+    @Column(name = "NEW_TRANSACTION_PRICE_PER_M2")
+    private BigDecimal newTransactionPricePerM2;
+
+    @Column(name = "NEW_TRANSACTION_LEVEL")
+    private int newTransactionLevel;
+
+    @Column(name = "NEW_TRANSACTION_PARKING_SPOT")
+    private String newTransactionParkingSpot;
+
+    @Column(name = "NEW_TRANSACTION_STANDARD_LEVEL")
+    private String newTransactionStandardLevel;
+
+    @Column(name = "NEW_TRANSACTION_CONSTRUCTION_YEAR")
+    private String newTransactionConstructionYear;
+
+    @Column(name = "NEW_TRANSACTION_CONSTRUCTION_YEAR_CATEGORY")
+    private int newTransactionConstructionYearCategory;
+
+    public NewTransaction() {
+    }
+
+    public NewTransaction(User newTransactionUserId, String newTransactionDescription, String newTransactionSale, String newTransactionImportant, LocalDate newTransactionDataTransaction, String newTransactionCity, String newTransactionDistrict, String newTransactionStreet, String newTransactionTypeOfMarket, BigDecimal newTransactionPrice, BigDecimal newTransactionFlatArea, BigDecimal newTransactionPricePerM2, int newTransactionLevel, String newTransactionParkingSpot, String newTransactionStandardLevel, String newTransactionConstructionYear, int newTransactionConstructionYearCategory) {
+        this.newTransactionUserId = newTransactionUserId;
+        this.newTransactionDescription = newTransactionDescription;
+        this.newTransactionSale = newTransactionSale;
+        this.newTransactionImportant = newTransactionImportant;
+        this.newTransactionDataTransaction = newTransactionDataTransaction;
+        this.newTransactionCity = newTransactionCity;
+        this.newTransactionDistrict = newTransactionDistrict;
+        this.newTransactionStreet = newTransactionStreet;
+        this.newTransactionTypeOfMarket = newTransactionTypeOfMarket;
+        this.newTransactionPrice = newTransactionPrice;
+        this.newTransactionFlatArea = newTransactionFlatArea;
+        this.newTransactionPricePerM2 = newTransactionPricePerM2;
+        this.newTransactionLevel = newTransactionLevel;
+        this.newTransactionParkingSpot = newTransactionParkingSpot;
+        this.newTransactionStandardLevel = newTransactionStandardLevel;
+        this.newTransactionConstructionYear = newTransactionConstructionYear;
+        this.newTransactionConstructionYearCategory = newTransactionConstructionYearCategory;
+    }
+
+    public int getNewTransactionId() {
+        return newTransactionId;
+    }
+
+    public void setNewTransactionId(int newTransactionId) {
+        this.newTransactionId = newTransactionId;
+    }
+
+    public User getNewTransactionUserId() {
+        return newTransactionUserId;
+    }
+
+    public void setNewTransactionUserId(User newTransactionUserId) {
+        this.newTransactionUserId = newTransactionUserId;
+    }
+
+    public String getNewTransactionDescription() {
+        return newTransactionDescription;
+    }
+
+    public void setNewTransactionDescription(String newTransactionDescription) {
+        this.newTransactionDescription = newTransactionDescription;
+    }
+
+    public String getNewTransactionSale() {
+        return newTransactionSale;
+    }
+
+    public void setNewTransactionSale(String newTransactionSale) {
+        this.newTransactionSale = newTransactionSale;
+    }
+
+    public String getNewTransactionImportant() {
+        return newTransactionImportant;
+    }
+
+    public void setNewTransactionImportant(String newTransactionImportant) {
+        this.newTransactionImportant = newTransactionImportant;
+    }
+
+    public LocalDate getNewTransactionDataTransaction() {
+        return newTransactionDataTransaction;
+    }
+
+    public void setNewTransactionDataTransaction(LocalDate newTransactionDataTransaction) {
+        this.newTransactionDataTransaction = newTransactionDataTransaction;
+    }
+
+    public String getNewTransactionCity() {
+        return newTransactionCity;
+    }
+
+    public void setNewTransactionCity(String newTransactionCity) {
+        this.newTransactionCity = newTransactionCity;
+    }
+
+    public String getNewTransactionDistrict() {
+        return newTransactionDistrict;
+    }
+
+    public void setNewTransactionDistrict(String newTransactionDistrict) {
+        this.newTransactionDistrict = newTransactionDistrict;
+    }
+
+    public String getNewTransactionStreet() {
+        return newTransactionStreet;
+    }
+
+    public void setNewTransactionStreet(String newTransactionStreet) {
+        this.newTransactionStreet = newTransactionStreet;
+    }
+
+    public String getNewTransactionTypeOfMarket() {
+        return newTransactionTypeOfMarket;
+    }
+
+    public void setNewTransactionTypeOfMarket(String newTransactionTypeOfMarket) {
+        this.newTransactionTypeOfMarket = newTransactionTypeOfMarket;
+    }
+
+    public BigDecimal getNewTransactionPrice() {
+        return newTransactionPrice;
+    }
+
+    public void setNewTransactionPrice(BigDecimal newTransactionPrice) {
+        this.newTransactionPrice = newTransactionPrice;
+    }
+
+    public BigDecimal getNewTransactionFlatArea() {
+        return newTransactionFlatArea;
+    }
+
+    public void setNewTransactionFlatArea(BigDecimal newTransactionFlatArea) {
+        this.newTransactionFlatArea = newTransactionFlatArea;
+    }
+
+    public BigDecimal getNewTransactionPricePerM2() {
+        return newTransactionPricePerM2;
+    }
+
+    public void setNewTransactionPricePerM2(BigDecimal newTransactionPricePerM2) {
+        this.newTransactionPricePerM2 = newTransactionPricePerM2;
+    }
+
+    public int getNewTransactionLevel() {
+        return newTransactionLevel;
+    }
+
+    public void setNewTransactionLevel(int newTransactionLevel) {
+        this.newTransactionLevel = newTransactionLevel;
+    }
+
+    public String getNewTransactionParkingSpot() {
+        return newTransactionParkingSpot;
+    }
+
+    public void setNewTransactionParkingSpot(String newTransactionParkingSpot) {
+        this.newTransactionParkingSpot = newTransactionParkingSpot;
+    }
+
+    public String getNewTransactionStandardLevel() {
+        return newTransactionStandardLevel;
+    }
+
+    public void setNewTransactionStandardLevel(String newTransactionStandardLevel) {
+        this.newTransactionStandardLevel = newTransactionStandardLevel;
+    }
+
+    public String getNewTransactionConstructionYear() {
+        return newTransactionConstructionYear;
+    }
+
+    public void setNewTransactionConstructionYear(String newTransactionConstructionYear) {
+        this.newTransactionConstructionYear = newTransactionConstructionYear;
+    }
+
+    public int getNewTransactionConstructionYearCategory() {
+        return newTransactionConstructionYearCategory;
+    }
+
+    public void setNewTransactionConstructionYearCategory(int newTransactionConstructionYearCategory) {
+        this.newTransactionConstructionYearCategory = newTransactionConstructionYearCategory;
+    }
+}
---
+++ web/src/main/java/com/infoshareacademy/jbusters/web/user/ShowNewTransaction.java
@@ -0,0 +1,85 @@
+package com.infoshareacademy.jbusters.web.user;
+
+
+import com.infoshareacademy.jbusters.dao.NewTransactionDao;
+import com.infoshareacademy.jbusters.dao.UserDao;
+import com.infoshareacademy.jbusters.freemarker.TemplateProvider;
+import com.infoshareacademy.jbusters.model.NewTransaction;
+import com.infoshareacademy.jbusters.model.User;
+import com.infoshareacademy.jbusters.web.ValuationServlet;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@WebServlet("/show-transaction")
+public class ShowNewTransaction extends HttpServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ShowNewTransaction.class);
+    private static final String TEMPLATE_NAME = "user-show-new-transaction";
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Inject
+    private UserDao userDao;
+
+    @Inject
+    private NewTransactionDao newTransactionDao;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+
+        final PrintWriter writer = resp.getWriter();
+        Map<String, Object> model = new HashMap<>();
+
+        HttpSession session = req.getSession();
+        String sessionEmail = (String) session.getAttribute("userEmail");
+        String sessionName = (String) session.getAttribute("userName");
+
+
+        List<User> listUsers = userDao.findAll();
+        List<User> emailList = listUsers.stream()
+                .filter(e -> e.getUserEmail().equals(sessionEmail))
+                .collect(Collectors.toList());
+
+        int userId = emailList.get(0).getUserId();
+
+        List<NewTransaction> newTransactionList = newTransactionDao.findAll();
+
+        List<NewTransaction> userTransaction  = newTransactionList.stream()
+                .filter(t -> t.getNewTransactionUserId().getUserId() == userId)
+                .collect(Collectors.toList());
+
+        Template template;
+        template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
+
+        model.put("sessionEmail", sessionEmail);
+        model.put("sessionName", sessionName);
+        model.put("trans", userTransaction);
+        model.put("size", userTransaction.size());
+
+        try {
+            template.process(model, writer);
+            LOG.info("Load list newTransaction");
+        } catch (TemplateException e) {
+            LOG.error("Failed load list newTransaction");
+        }
+    }
+}
